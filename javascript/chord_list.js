e0e7f6cdcbcce46db90940559b4ba1901358c4f8
Jorge Mateus <jorgemateus1995@gmail.com>
2023-12-16 16:56:03 -0500

    Se agrega la funcionalidad de renderizar accodes con cejilla y se implementa la base de datos basica con el localStorage

--- javascript/chord_list.js
+++ javascript/chord_list.js
@@ -17,9 +17,10 @@ const allFrets = [
   firstStringFrets,
 ];
 
-let currentRoot = "";
-let currentVariation = "";
+let currentRoot = "C";
+let currentVariation = "major";
 let notesArray = [];
+let chordData;
 const colorMap = {
   0: "#e67e22",
   1: "#3498db",
@@ -28,8 +29,32 @@ const colorMap = {
   4: "#f1c40f",
 };
 
-const testFrets = [-1, 3, 2, 0, 1, 0];
-const testFingering = [0, 3, 2, 0, 1, 0];
+//Testing
+
+const testFrets = [1, 3, 3, 1, 1, 1];
+const testFingering = [1, 3, 4, 1, 1, 1];
+const barre = true;
+const capo = true;
+const baseFret = 8;
+
+//Inicializar base de datos
+
+const initDB = function (rootNote) {
+  fetch("../src/guitar.json")
+    .then((response) => response.json())
+    .then((data) => {
+      chordData = {
+        ...data[rootNote].filter((chord) => chord.suffix === currentVariation),
+      };
+      chordData[0].positions.forEach((chord, index) => {
+        console.log(chord);
+        localStorage.setItem(`Position ${index}`, JSON.stringify(chord));
+      });
+    })
+    .catch((error) => {
+      console.error("Error reading JSON file:", error);
+    });
+};
 
 //Function to render notes
 
@@ -49,19 +74,52 @@ const createNoteElements = function (fingeringArray, fretArray) {
   });
 };
 
-const insertNoteElements = function (fretNumber, stringFrets, index) {
+const insertNoteElements = function (
+  fretNumber,
+  stringFrets,
+  index,
+  barre,
+  capo,
+  baseFret
+) {
   let matchingFret;
   if (fretNumber === -1) {
     matchingFret = [...stringFrets].find((fret) => fret.dataset.number === "0");
-  } else {
+  } else if (!baseFret && !capo && !barre) {
     matchingFret = [...stringFrets].find(
       (fret) => fret.dataset.number === fretNumber.toString()
     );
+  } else {
+    matchingFret = [...stringFrets].find(
+      (fret) => fret.dataset.number * 1 === fretNumber + baseFret
+    );
   }
-  debugger;
   if (matchingFret) {
     matchingFret.innerHTML = notesArray[index];
   }
+};
+
+const insertBarre = function (barre, baseFret) {
+  document.addEventListener("DOMContentLoaded", function () {
+    if (barre && baseFret >= 0) {
+      // Crear pseudo elemento
+      const barreElement = document.createElement("before");
+
+      // Agregar estilos
+      barreElement.style.cssText =
+        "content: ''; background-color: var(--primary-blue); opacity: 0.8; position: absolute; height: 26rem; width: 2.5rem; border-radius: 20px; transform: translateX(-2rem) translateY(6rem);";
+
+      // Seleccionar el padre
+      const parentElement = document.querySelector(
+        `.fret-numbers li:nth-child(${baseFret + 3})`
+      );
+
+      if (parentElement) {
+        // Agregar el pseudo elemento
+        parentElement.appendChild(barreElement);
+      }
+    }
+  });
 };
 
 //Basic toggle functionality for picklist
@@ -71,7 +129,10 @@ rootNotes.forEach((note) =>
     rootNotes.forEach((n) => n.classList.remove("active-root"));
     chordVariations.forEach((v) => v.classList.remove("active-variation"));
     e.target.classList.toggle("active-root");
+    //Borrar local storage antes de actualizar variable
+    localStorage.clear();
     currentRoot = e.target.textContent;
+    initDB(currentRoot);
   })
 );
 
@@ -80,15 +141,17 @@ chordVariations.forEach((variation) =>
     chordVariations.forEach((v) => v.classList.remove("active-variation"));
     e.target.classList.toggle("active-variation");
     currentVariation = e.target.textContent;
+    console.log(JSON.parse(localStorage.getItem(currentRoot)));
   })
 );
 
-document.querySelector(".logo-container").addEventListener("click", () => {
-  firstStringFrets.forEach((f) => console.log(f.dataset.number));
-});
+localStorage.clear();
+initDB(currentRoot);
 
 createNoteElements(testFingering, testFrets);
 
+insertBarre(barre, baseFret);
+
 for (let i = 0; i <= 6; i++) {
-  insertNoteElements(testFrets[i], allFrets[i], i);
+  insertNoteElements(testFrets[i], allFrets[i], i, barre, capo, baseFret);
 }
